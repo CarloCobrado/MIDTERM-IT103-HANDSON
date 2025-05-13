62b0ff79ffa46595f950c636deefa7bddabe3733
CarloCobrado <carlo.cobrado@jmc.edu.ph>
2025-05-13 15:25:16 +0800

    Update1 IT103.java
    Just cleaned up indents and spacing

--- IT103.java
+++ IT103.java
@@ -3,28 +3,28 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class IT103 {
-    public static void calculateAverageGrade() {
+	public static void calculateAverageGrade() {
 		File file = new File("Grades.txt");
 		float Sum = 0;
 		int StudentCount = 0;
-
-			try (Scanner scanner = new Scanner(file)) {
-				while (scanner.hasNext()) {
-					if (scanner.hasNextInt()) {
-						int Grades = scanner.nextInt();
-						Sum += Grades;
-						StudentCount++;
-					}
-					else {
-                    	scanner.next();
-                	}
+	
+		try (Scanner scanner = new Scanner(file)) {
+			while (scanner.hasNext()) {
+				if (scanner.hasNextInt()) {
+					int Grades = scanner.nextInt();
+					Sum += Grades;
+					StudentCount++;
 				}
-				double TotalAverage = Sum / StudentCount;
-				System.out.printf("Average Grade: " + "%.2f\n", TotalAverage);
-				scanner.close();
+				else {
+	                    		scanner.next();
+	                	}
 			}
-			catch (FileNotFoundException e) {
-				System.err.println("File not found.");
+			double TotalAverage = Sum / StudentCount;
+			System.out.printf("Average Grade: " + "%.2f\n", TotalAverage);
+			scanner.close();
+		}
+		catch (FileNotFoundException e) {
+			System.err.println("File not found.");
 		}
 	}
 
@@ -35,10 +35,10 @@ public class IT103 {
 			while (scanner.hasNext()) {
 				if (scanner.hasNextLine()) {
 					String Student = scanner.next();
-						if (scanner.hasNextInt()) {
-							int Grades = scanner.nextInt();
-							System.out.printf ("%s: %d\n", Student, Grades);
-						}
+					if (scanner.hasNextInt()) {
+						int Grades = scanner.nextInt();
+						System.out.printf ("%s: %d\n", Student, Grades);
+					}
 				}
 				else {
 					scanner.next();
@@ -50,6 +50,7 @@ public class IT103 {
 			System.err.println("File not found.");
 		}
 	}
+	
 	public static void main(String[] args) {
 		printStudentGrades();
 		System.out.println();
